23d84a51034ad7ea84e862f5a6ddd3bb9ac306e9
Stenio Vilar <stenio.vilar@gmail.com>
2025-02-26 17:48:39 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array1 = {-1,0,5,9};
+        int[] array2 = {2,8,11,17};
+        int inputSum = 7;
+
+        System.out.println("Result Positions: " + Arrays.toString(checkPairSorted(array1,array2,inputSum)));
+
+    }
+
+    private static int[] checkPairBruteForce(int[] array1, int[] array2, int inputSum) {
+
+        for(int x =0; x < array1.length; x++){
+            for(int j =0; j < array2.length; j++){
+                if(array1[x] + array2[j] == inputSum){
+                    return new int[] {x,j};
+                }
+            }
+        }
+        return new int[] {-1,-1};
+    }
+
+    private static int[] checkPairSorted(int[] array1, int[] array2, int inputSum) {
+        int x = 0;
+        int j = array2.length - 1;
+
+        while(x < array1.length && j >= 0){
+            int sum = array1[x] + array2[j];
+            if(sum == inputSum){
+                return new int[] {x,j};
+            }else if(sum < inputSum){
+                x++;
+            }else{
+                j--;
+            }
+        }
+        return new int[] {-1,-1};
+    }
+
+
+}
